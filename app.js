42dbdc9acaec2647be95ca7414ef62cdf38003a9
SergioLabraLopez <slabralopez@gmail.com>
2025-01-14 16:41:13 +0100

    primera clase, función watch en el fichero

---
+++ app.js
@@ -0,0 +1,4 @@
+// para que el fichero esté compiladon constantemente escribir por consola: "tsc app.ts --watch"
+console.log("hello world");
+var item = "Bolsa";
+var num = 2;
---
+++ app.ts
@@ -0,0 +1,7 @@
+// para que el fichero esté compiladon constantemente escribir por consola: "tsc app.ts --watch"
+
+console.log("hello world");
+
+let item = "Bolsa";
+
+let num = 2;
